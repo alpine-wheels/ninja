f4342417f711d94b6c65f82b6d31df747b1470b1
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-06-04 22:56:32 -0500

    ninja 1.11.1.1 (#1)

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-# apk add --no-cache gcc musl-dev
+apk add --no-cache cmake g++ gcc make musl-dev
 
 # build
-pip wheel --no-deps --requirement requirements.txt
+CXX="/usr/bin/g++" pip wheel --extra-index-url https://alpine-wheels.github.io/index --no-deps --requirement requirements.txt
