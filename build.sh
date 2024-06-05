46ac68d1651d264b152227d873bdadcdfe997d12
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-06-04 22:44:59 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-apk add --no-cache cmake gcc make musl-dev
+apk add --no-cache cmake g++ gcc make musl-dev
 
 # build
-pip wheel --extra-index-url https://alpine-wheels.github.io/index --no-deps --requirement requirements.txt
+CXX="/usr/bin/g++" pip wheel --extra-index-url https://alpine-wheels.github.io/index --no-deps --requirement requirements.txt
